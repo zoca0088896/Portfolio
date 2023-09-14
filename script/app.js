a59b624237e8bd3ad8bbde41c0a62cd73a4927b6
Roland Huang <eric68937@gmail.com>
2023-09-14 21:02:05 +0800

    Portfolio ver.0

---
+++ script/app.js
@@ -0,0 +1,18 @@
+let t1 = gsap.timeline({ repeat: -1 });
+t1.to(".QRcode3", { opacity: 0, duration: 1 }, 4);
+t1.to(".QRcode2", { opacity: 0, duration: 1 }, 9);
+t1.to(".QRcode1", { opacity: 0, duration: 1 }, 14);
+t1.to(".QRcode3", { opacity: 1, duration: 1 }, 14);
+let header = document.querySelector("header");
+let scrollStart = 0;
+let scrollEnd;
+window.addEventListener("scroll", (e) => {
+  scrollEnd = window.scrollY;
+  if (scrollEnd - scrollStart > 0) {
+    scrollStart = scrollEnd;
+    header.style.top = "-100px";
+  } else {
+    scrollStart = scrollEnd;
+    header.style.top = "0px";
+  }
+});
